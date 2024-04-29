d0c255a6153487d4bb7b02b63e39fa41b454adf8
Donghyeok Tak <tdh8316@naver.com>
2024-04-29 11:18:45 +0900

    Add README.md and setup.py

---
+++ README.md
@@ -0,0 +1,9 @@
+# Overview
+Torch thunder
+
+## Installation
+```bash
+$ conda create -n torch_thunder python=3.11
+$ activate torch_thunder
+(torch_thunder)$ python -m pip install git+https://github.com/tdh8316/torch_thunder/
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="torch_thunder",
+    version="0.1.0",
+    packages=["torch_thunder"],
+    url="https://github.com/tdh8316/torch_thunder/",
+    license="MIT License",
+    author="Donghyeok Tak",
+    author_email="tdh8316@kw.ac.kr",
+    description="Feel the thunder",
+    python_requires=">=3.10",
+)
