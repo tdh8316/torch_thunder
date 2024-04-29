6697e39818df471ad4add621ab9cd2a766710df8
Donghyeok Tak <tdh8316@naver.com>
2024-04-29 12:38:37 +0900

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="torch_thunder",
-    version="0.1.0",
+    version="0.1.1",
     packages=["torch_thunder"],
     url="https://github.com/tdh8316/torch_thunder/",
     license="MIT License",
